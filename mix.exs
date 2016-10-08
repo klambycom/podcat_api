30c4a9060a49eee1e7e050ca39dc2c6a799aec6e
klambycom <christian@klamby.com>
2016-10-08 23:56:06 +0200

    Install exrm

--- mix.exs
+++ mix.exs
@@ -34,7 +34,9 @@ defmodule PodcatApi.Mixfile do
         :timex,
         :timex_ecto,
         :corsica,
-        :poolboy
+        :poolboy,
+        :guardian,
+        :mogrify
       ]
     ]
   end
@@ -63,7 +65,8 @@ defmodule PodcatApi.Mixfile do
       {:timex_ecto, "~> 3.0"},
       {:ex_doc, "~> 0.12", only: :dev},
       {:corsica, "~> 0.4"},
-      {:poolboy, "~> 1.5"}
+      {:poolboy, "~> 1.5"},
+      {:exrm, "~> 1.0"}
     ]
   end
 
