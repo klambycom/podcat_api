0e96ac6fa3df31ba8dc9482b5fabf46ef187355a
klambycom <christian@klamby.com>
2016-10-26 22:10:53 +0200

    Return empty user if the user is not signed in

--- web/resolver/user.ex
+++ web/resolver/user.ex
@@ -2,7 +2,9 @@ defmodule PodcatApi.Resolver.User do
   alias PodcatApi.{Repo, User, Subscription}
 
   @doc """
-  Find user from id, current user or subscription.
+  Find user from id, current user or subscription. Return a
+  empty user if no id is provided and the user is not signed
+  in.
   """
   def find(%{id: id}, _) do
     case Repo.get(User, id) do
@@ -14,12 +16,21 @@ defmodule PodcatApi.Resolver.User do
   def find(%{}, %{source: %Subscription{} = subscription}),
     do: {:ok, subscription.user}
 
-  def find(%{}, %{context: %{conn: conn}}),
-    do: {:ok, Guardian.Plug.current_resource(conn)}
+  def find(%{}, %{context: %{conn: conn}}) do
+    case Guardian.Plug.current_resource(conn) do
+      nil  -> {:ok, %User{id: 0}}
+      user -> {:ok, user}
+    end
+  end
 
   @doc """
-  Avatar from gravatar.
+  Avatar from gravatar. Empty string if the user is empty
+  (or not signed in).
   """
-  def image(%{size: size}, %{source: user}),
-    do: {:ok, User.gravatar_url(user, size)}
+  def image(%{size: size}, %{source: user}) do
+    case user.id do
+      0 -> {:ok, ""}
+      _ -> {:ok, User.gravatar_url(user, size)}
+    end
+  end
 end
